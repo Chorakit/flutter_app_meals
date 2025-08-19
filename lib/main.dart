0afb15fece703e725e537a3cdaee98ca376d2ca4
Chorakit <jorakit.work@gmail.com>
2025-08-19 14:35:15 +0700

    ีupdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_meals/categories.dart';
+import 'package:flutter_app_meals/screens/categories.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final theme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
+    seedColor: const Color.fromARGB(255, 131, 57, 0),
+  ),
+  textTheme: GoogleFonts.latoTextTheme(),
+);
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +21,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const CategoriesScreen());
+    return MaterialApp(theme: theme, home: const CategoriesScreen());
   }
 }
--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_meals/data/dummy_data.dart';
+import 'package:flutter_app_meals/models/categorty.dart';
+import 'package:flutter_app_meals/widgets/dategory_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -11,16 +14,13 @@ class CategoriesScreen extends StatelessWidget {
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
         ),
         children: [
-          Text('1'),
-          Text('1'),
-          Text('1'),
-          Text('1'),
-          Text('1'),
-          Text('1'),
+          //...availableCategories.map((category) => CategoryGridItem(category: category)).toList();
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
         ],
       ),
     );
