40bf7bb8668c98271a715adaedb54b93ac5de963
Chorakit <jorakit.work@gmail.com>
2025-08-19 13:35:57 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_app_meals/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_app_meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/categories.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('pick your category')),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        children: [
+          Text('1'),
+          Text('1'),
+          Text('1'),
+          Text('1'),
+          Text('1'),
+          Text('1'),
+        ],
+      ),
+    );
+  }
+}
